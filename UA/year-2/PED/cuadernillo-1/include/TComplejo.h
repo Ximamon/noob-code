7d0c3a1959663e41486768c3172e49db55dbda0d
Julián Hinojosa <jhg45@alu.ua.es>
2026-02-11 11:43:25 +0100

    new ADA Practice

--- UA/year-2/PED/cuadernillo-1/include/TComplejo.h
+++ UA/year-2/PED/cuadernillo-1/include/TComplejo.h
@@ -15,27 +15,27 @@ public:
     ~TComplejo();                       //Destructor
     TComplejo& operator= (const TComplejo&);  // Sobrecarga del operador asignación
 
-    TComplejo operator+ (TComplejo&);
-    TComplejo operator- (TComplejo&);
-    TComplejo operator* (TComplejo&);
-    TComplejo operator+ (double);
-    TComplejo operator- (double);
-    TComplejo operator* (double);
+    TComplejo operator+ (const TComplejo&) const;
+    TComplejo operator- (const TComplejo&) const;
+    TComplejo operator* (const TComplejo&) const;
+    TComplejo operator+ (double) const;
+    TComplejo operator- (double) const;
+    TComplejo operator* (double) const;
 
     bool operator== (const TComplejo&) const;   // IGUALDAD de números complejos
-    bool operator!= (TComplejo&);               // DESIGUALDAD de números complejos
+    bool operator!= (const TComplejo&) const;   // DESIGUALDAD de números complejos
     double Re() const;                          // Devuelve PARTE REAL
     double Im() const;                          // Devuelve PARTE IMAGINARIA
     void Re(double);                            // Modifica PARTE REAL
     void Im(double);                            // Modifica PARTE IMAGINARIA
-    double Arg(void);                           // Calcula el Argumento (en Radianes)
-    double Mod(void);                           // Calcula el Módulo
+    double Arg() const;                         // Calcula el Argumento (en Radianes)
+    double Mod() const;                         // Calcula el Módulo
 
     // Sobrecarga del operador SALIDA
     friend std::ostream & operator<<(std::ostream &, const  TComplejo &);
-    friend TComplejo operator+ (double , TComplejo&);
-    friend TComplejo operator- (double , TComplejo&);
-    friend TComplejo operator* (double , TComplejo&);
+    friend TComplejo operator+ (double , const TComplejo&);
+    friend TComplejo operator- (double , const TComplejo&);
+    friend TComplejo operator* (double , const TComplejo&);
 private:
     double real;
     double imaginario;
--- UA/year-2/PED/cuadernillo-1/lib/TComplejo.cpp
+++ UA/year-2/PED/cuadernillo-1/lib/TComplejo.cpp
@@ -4,7 +4,7 @@
 
 #include "../include/TComplejo.h"
 #include <iostream>
-#include <iosfwd>
+#include <cmath>
 
 
 TComplejo::TComplejo() {
@@ -34,16 +34,51 @@ TComplejo::~TComplejo() {
 
 TComplejo& TComplejo::operator= (const TComplejo &other) {
     if (this != &other) {
-        (*this).~TComplejo();
         this->real = other.real;
         this->imaginario = other.imaginario;
     }
     return *this;
 }
 
+TComplejo TComplejo::operator+ (const TComplejo& other) const {
+    TComplejo res(this->real + other.real, this->imaginario + other.imaginario);
+    return res;
+}
+
+TComplejo TComplejo::operator- (const TComplejo& other) const {
+    TComplejo res(this->real - other.real, this->imaginario - other.imaginario);
+    return res;
+}
+
+TComplejo TComplejo::operator* (const TComplejo& other) const {
+    // Aplicando (ac - bd) + (ad + ac)
+    double realPart = (this->real * other.real) - (this->imaginario * other.imaginario);
+    double imagPart = (this->real * other.imaginario) + (this->imaginario * other.real);
+    TComplejo res(realPart, imagPart);
+    return res;
+}
+
+TComplejo TComplejo::operator+ (double d) const {
+    TComplejo temp(d);
+    return (*this + temp);
+}
+
+TComplejo TComplejo::operator- (double d) const {
+    TComplejo temp(d);
+    return (*this - temp);
+}
+
+TComplejo TComplejo::operator* (double d) const {
+    TComplejo temp(d);
+    return (*this * temp);
+}
+
 bool TComplejo::operator==(const TComplejo &other) const {
-    bool temp = (real == other.real && imaginario == other.imaginario) ? true : false;
-    return temp;
+    return (this->real == other.real && this->imaginario == other.imaginario);
+}
+
+bool TComplejo::operator!=(const TComplejo &other) const {
+    return (this->real != other.real || this->imaginario != other.imaginario);
 }
 
 double TComplejo::Re() const {
@@ -62,7 +97,34 @@ void TComplejo::Im(const double im) {
     this->imaginario = im;
 }
 
+double TComplejo::Arg() const {
+    // atan2(y, x) calcula el arcotangente devolviendo el cuadrante correcto
+    double a = atan2(this->imaginario, this->real);
+    return a;
+}
+
+double TComplejo::Mod() const {
+    // Almacenamos en una variable para evitar problemas de precision
+    double a = sqrt(pow(this->real, 2) + pow(this->imaginario, 2));
+    return a;
+}
+
 std::ostream &operator<<(std::ostream &os, const TComplejo &complejo) {
-    os << "(" << complejo.Re() << " " << complejo.Im() << ")" << std::endl;
+    os << "(" << complejo.Re() << " " << complejo.Im() << ")";
     return os;
+}
+
+TComplejo operator+(double d, const TComplejo &other) {
+    TComplejo temp(d);
+    return temp + other;
+}
+
+TComplejo operator-(double d, const TComplejo &other) {
+    TComplejo temp(d);
+    return temp - other;
+}
+
+TComplejo operator*(double d, const TComplejo &other) {
+    TComplejo temp(d);
+    return temp * other;
 }
