878a67b6ea40035001ffd116550f53d25fc411ed
Julián Hinojosa <jhg45@alu.ua.es>
2026-02-17 19:03:38 +0100

    new rule and comments

--- UA/year-2/PED/cuadernillo-1/lib/tcomplejo.cpp
+++ UA/year-2/PED/cuadernillo-1/lib/tcomplejo.cpp
@@ -7,53 +7,55 @@
 #include <cmath>
 
 
-TComplejo::TComplejo() {
-    this->real = 0;
-    this->imaginario = 0;
-}
+// ------------------- Forma canonica -------------------
 
-TComplejo::TComplejo(double re) {
-    this->real = re;
-    this->imaginario = 0;
-}
-
+// Constructor unico (cubre constructor por defecto, con 1 arg y 2 arg)
 TComplejo::TComplejo(double re, double im) {
-    this->real = re;
-    this->imaginario = im;
+    this->re = re;
+    this->im = im;
 }
 
+// Constructor de copia
 TComplejo::TComplejo(const TComplejo &other) {
-    this->real = other.real;
-    this->imaginario = other.imaginario;
+    this->re = other.re;
+    this->im = other.im;
 }
 
+// Destructor
 TComplejo::~TComplejo() {
-    this->real = 0;
-    this->imaginario = 0;
+    this->re = 0;
+    this->im = 0;
 }
 
+// Operardor de Asignacion
 TComplejo& TComplejo::operator= (const TComplejo &other) {
     if (this != &other) {
-        this->real = other.real;
-        this->imaginario = other.imaginario;
+        this->re = other.re;
+        this->im = other.im;
     }
     return *this;
 }
 
+// ------------------------------------------------------
+
+// ------------------- Operadorres Aritmeticos (clase) -------------------
+
 TComplejo TComplejo::operator+ (const TComplejo& other) const {
-    TComplejo res(this->real + other.real, this->imaginario + other.imaginario);
+    // Suma: (a+c) + (b+d)i
+    TComplejo res(this->re + other.re, this->im + other.im);
     return res;
 }
 
 TComplejo TComplejo::operator- (const TComplejo& other) const {
-    TComplejo res(this->real - other.real, this->imaginario - other.imaginario);
+    // Resta: (a-c) + (b-d)i
+    TComplejo res(this->re - other.re, this->im - other.im);
     return res;
 }
 
 TComplejo TComplejo::operator* (const TComplejo& other) const {
-    // Aplicando (ac - bd) + (ad + ac)
-    double realPart = (this->real * other.real) - (this->imaginario * other.imaginario);
-    double imagPart = (this->real * other.imaginario) + (this->imaginario * other.real);
+    // Multiplicación: (ac - bd) + (ad + bc)i
+    double realPart = (this->re * other.re) - (this->im * other.im);
+    double imagPart = (this->re * other.im) + (this->im * other.re);
     TComplejo res(realPart, imagPart);
     return res;
 }
@@ -73,41 +75,57 @@ TComplejo TComplejo::operator* (double d) const {
     return (*this * temp);
 }
 
+// -----------------------------------------------------------------------
+
+// ------------------- Comparacion -------------------
+
 bool TComplejo::operator==(const TComplejo &other) const {
-    return (this->real == other.real && this->imaginario == other.imaginario);
+    return (this->re == other.re && this->im == other.im);
 }
 
 bool TComplejo::operator!=(const TComplejo &other) const {
-    return (this->real != other.real || this->imaginario != other.imaginario);
+    return (this->re != other.re || this->im != other.im);
 }
 
+// ---------------------------------------------------
+
+// ------------------- Getter / Setters -------------------
+
 double TComplejo::Re() const {
-    return this->real;
+    return this->re;
 }
 
 double TComplejo::Im() const {
-    return this->imaginario;
+    return this->im;
 }
 
 void TComplejo::Re(const double re) {
-    this->real = re;
+    this->re = re;
 }
 
 void TComplejo::Im(const double im) {
-    this->imaginario = im;
+    this->im = im;
 }
+
+// --------------------------------------------------------
+
+// ------------------- Calculo (Arg y Mod) -------------------
 
 double TComplejo::Arg() const {
     // atan2(y, x) calcula el arcotangente devolviendo el cuadrante correcto
-    double a = atan2(this->imaginario, this->real);
+    double a = atan2(this->im, this->re);
     return a;
 }
 
 double TComplejo::Mod() const {
     // Almacenamos en una variable para evitar problemas de precision
-    double a = sqrt(pow(this->real, 2) + pow(this->imaginario, 2));
+    double a = sqrt(pow(this->re, 2) + pow(this->im, 2));
     return a;
 }
+
+// -----------------------------------------------------------
+
+// ------------------- Funciones amigas -------------------
 
 std::ostream &operator<<(std::ostream &os, const TComplejo &complejo) {
     os << "(" << complejo.Re() << " " << complejo.Im() << ")";
@@ -128,3 +146,5 @@ TComplejo operator*(double d, const TComplejo &other) {
     TComplejo temp(d);
     return temp * other;
 }
+
+// --------------------------------------------------------
